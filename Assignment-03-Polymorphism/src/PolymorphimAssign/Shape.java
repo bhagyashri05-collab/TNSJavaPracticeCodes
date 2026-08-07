43a374d4e9a46becb69d2f02fbaee6fcec550033
bhagyashri05-collab <bhagyashriwale05@gmail.com>
2026-08-07 11:14:49 +0530

    Add files via upload

--- Assignment-03-Polymorphism/src/PolymorphimAssign/Shape.java
+++ Assignment-03-Polymorphism/src/PolymorphimAssign/Shape.java
@@ -1,1 +1,53 @@
+package PolymorphimAssign;
 
+public class Shape {
+
+    public void draw() {
+        System.out.println("Drawing Shape");
+    }
+
+    public void erase() {
+        System.out.println("Erasing Shape");
+    }
+
+    public static void main(String[] args) {
+
+        Shape[] shapes = {
+            new Shape(),
+            new Triangle(),
+            new Square()
+        };
+
+        for (Shape s : shapes) {
+            s.draw();
+            s.erase();
+            System.out.println("********");
+        }
+    }
+}
+
+class Triangle extends Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Triangle");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Erasing Triangle");
+    }
+}
+
+class Square extends Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Square");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Erasing Square");
+    }
+}
