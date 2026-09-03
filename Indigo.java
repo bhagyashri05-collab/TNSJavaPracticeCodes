c10b9f88fed244064abb642c849c1f83b457fa13
bhagyashri05-collab <bhagyashriwale05@gmail.com>
2026-09-03 22:35:24 +0530

    Add files via upload

---
+++ Indigo.java
@@ -0,0 +1,34 @@
+package interface_assignment;
+
+class Indigo implements Airfare {
+    private int hours;
+    private double costPerHour;
+
+    Indigo() {
+    }
+
+    Indigo(int hours, double costPerHour) {
+        this.hours = hours;
+        this.costPerHour = costPerHour;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public double getCostPerHour() {
+        return costPerHour;
+    }
+
+    public void setCostPerHour(double costPerHour) {
+        this.costPerHour = costPerHour;
+    }
+
+    public double calculateAmount() {
+        return hours * costPerHour * 8;
+    }
+}
